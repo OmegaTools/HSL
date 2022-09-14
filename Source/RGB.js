451cd59859690d7167dca3e8fde49d9808e8c237
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2022-09-14 15:36:51 -0400

    Refactored Code

--- Source/RGB.js
+++ Source/RGB.js
@@ -1,93 +1,80 @@
-/*
-    Copyright (c) 2021 JDK.FHWS@gmail.com
-*/
-
 
 const { min , max } = Math;
 
 
 /*
-    To Normalized
-*/
+ *  [ 0 - 255 ]  ➞  [ 0 - 1 ]
+ */
 
-function toNormalized(byte){
-  return byte / 255;
+function normalize(byte){
+    return byte / 255;
 }
 
 
-/*
-    To HSL
-*/
 
 function toHSL(input){
 
-  const [ red , green , blue ] = input.map(toNormalized);
+    const [ red , green , blue ] = input
+        .map(normalize);
 
-  const
-    minimun = min(red,green,blue),
-    maximum = max(red,green,blue);
+    const
+        minimun = min(red,green,blue) ,
+        maximum = max(red,green,blue) ;
 
-  const
-    chroma = maximum - minimun,
-    lightness = (maximum + minimun) * .5;
+    const
+        chroma = maximum - minimun,
+        lightness = (maximum + minimun) * .5;
 
-  return [ calcHue() , calcSaturation() , lightness ];
+    return [ calcHue() , calcSaturation() * 100 , lightness * 100 ];
 
 
 
-  /*
-      Calculate Hue
-  */
+    function calcHue(){
 
-  function calcHue(){
+        if(chroma === 0)
+            return 0;
 
-    if(chroma === 0)
-      return 0;
+        let hue;
 
-    let hue;
+        switch(maximum){
+        case red   : hue = (green - blue); break;
+        case green : hue = (blue - red);   break;
+        case blue  : hue = (red - green);  break;
+        }
 
-    switch(maximum){
-    case red   : hue = (green - blue); break;
-    case green : hue = (blue - red);   break;
-    case blue  : hue = (red - green);  break;
+        hue /= chroma;
+
+        switch(maximum){
+        case red   : hue += 0; break;
+        case green : hue += 2; break;
+        case blue  : hue += 4; break;
+        }
+
+        hue *= 60;
+        hue %= 360;
+
+        if(hue < 0)
+            hue += 360;
+
+        return hue;
     }
 
-    hue /= chroma;
 
-    switch(maximum){
-    case red   : hue += 0; break;
-    case green : hue += 2; break;
-    case blue  : hue += 4; break;
+    function calcSaturation(){
+        
+        if([ 0 , 1 ].includes(lightness))
+            return 0;
+
+        return (maximum - lightness) / min(lightness,1 - lightness);
     }
-
-    hue *= 60;
-    hue %= 360;
-
-    if(hue < 0)
-      hue += 360;
-
-    return hue;
-  }
-
-
-  /*
-      Calculate Saturation
-  */
-
-  function calcSaturation(){
-    if([ 0 , 1 ].includes(lightness))
-      return 0;
-
-    return (maximum - lightness) / min(lightness,1 - lightness);
-  }
 }
 
 
 /*
-    [ R , G , B ] -> [ H , S , L ]
-*/
+ *  [ R , G , B , (A) ] -> [ H , S , L , (A) ]
+ */
 
-module.exports = (...colors) => {
+export default function fromRGB(colors){
 
   if(colors.length === 1 || (typeof colors[2] !== 'number'))
     colors = colors[0];
--- Source/mod.ts
+++ Source/mod.ts
@@ -1,6 +1,13 @@
-/*
-    Copyright (c) 2021 JDK.FHWS@gmail.com
-*/
+
+import convertRGB from './RGB.js'
 
 
-exports.rgbToHSL = require('./RGB.js');
+/**
+ *  @brief Converts an RGB color to HSL
+ *  @param rgb Either ( R , G , B ) or ([ R , G , B ])
+ *  @note Alpha channel can be included and won't be altered.
+ */
+
+export function fromRGB ( ... rgb : number [] | number[][] ) : number [] {
+    return convertRGB ( rgb ) as number [];
+}
